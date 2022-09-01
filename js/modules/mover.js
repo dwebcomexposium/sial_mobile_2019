9e59ffc3eded436d1ed86094b3f12cae897e2344
Etienne <etienne.moureton@unflux.fr>
2022-09-01 11:28:08 +0200

    Update mover.js

--- js/modules/mover.js
+++ js/modules/mover.js
@@ -1,19 +1,9 @@
-// (function ($) {
-//   const baneer = document.querySelector('.evd-wrapper .block')
-//   const desc = document.querySelector('.evd-wrapper > .evd-desc')
-//   const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
-//   console.log(baneer, desc, child)
-//   desc.insertBefore(baneer, child)
-// })(jQuery);
-
-window.addEventListener('load',() => {
-  const target = document.querySelector('.evd-wrapper > .evd-desc');
-  const observer = new MutationObserver(() => {
-    const baneer = document.querySelector('.evd-wrapper .block')
-    const desc = document.querySelector('.evd-wrapper > .evd-desc')
-    const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
-    desc.insertBefore(baneer, child)
-  });
-  const config = { childList: true };
-  observer.observe(target, config);
+const target = document.querySelector('.evd-wrapper > .evd-desc');
+const observer = new MutationObserver(() => {
+  const baneer = document.querySelector('.evd-wrapper .block')
+  const desc = document.querySelector('.evd-wrapper > .evd-desc')
+  const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
+  desc.insertBefore(baneer, child)
 });
+const config = { childList: true };
+observer.observe(target, config);
