a729fb471b208b1330deb62b8374443bc38d3ee2
Etienne <etienne.moureton@unflux.fr>
2022-09-01 11:23:29 +0200

    Update mover.js

--- js/modules/mover.js
+++ js/modules/mover.js
@@ -9,7 +9,7 @@
 window.addEventListener('load',() => {
   const target = document.querySelector('.evd-wrapper > .evd-desc');
   const observer = new MutationObserver(() => {
-    const baneer = document.querySelector('.evd-wrapper .block')
+    const baneer = document.querySelector('.evd-wrapper > .block')
     const desc = document.querySelector('.evd-wrapper > .evd-desc')
     const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
     console.log(desc, baneer, child)
