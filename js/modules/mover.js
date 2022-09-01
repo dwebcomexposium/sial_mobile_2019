996ff404c4297bbb0cf6a7059f6092694f85a1be
Etienne <etienne.moureton@unflux.fr>
2022-09-01 10:59:41 +0200

    Update mover.js

--- js/modules/mover.js
+++ js/modules/mover.js
@@ -2,6 +2,7 @@ const moveElements = () => {
   const baneer = document.querySelector('.evd-wrapper .block')
   const desc = document.querySelector('.evd-wrapper > .evd-desc')
   const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
+  console.log(baneer, desc, child)
   desc.insertBefore(baneer, child)
 }
 
