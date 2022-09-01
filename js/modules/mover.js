04e554c6cdd2412dc98279fe325e41ad39158844
Etienne <etienne.moureton@unflux.fr>
2022-09-01 11:08:30 +0200

    Update mover.js

--- js/modules/mover.js
+++ js/modules/mover.js
@@ -1,7 +1,15 @@
-(function ($) {
+// (function ($) {
+//   const baneer = document.querySelector('.evd-wrapper .block')
+//   const desc = document.querySelector('.evd-wrapper > .evd-desc')
+//   const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
+//   console.log(baneer, desc, child)
+//   desc.insertBefore(baneer, child)
+// })(jQuery);
+
+window.addEventListener('load',() => {
   const baneer = document.querySelector('.evd-wrapper .block')
   const desc = document.querySelector('.evd-wrapper > .evd-desc')
   const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
   console.log(baneer, desc, child)
   desc.insertBefore(baneer, child)
-})(jQuery);
+});
