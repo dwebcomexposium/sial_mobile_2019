c260e6b7f3953b4da5458a25e8df43b342edffac
Etienne <etienne.moureton@unflux.fr>
2022-09-01 11:32:56 +0200

    Update mover.js

--- js/modules/mover.js
+++ js/modules/mover.js
@@ -1,9 +1,11 @@
-const target = document.querySelector('.evd-wrapper > .evd-desc');
+const target = document.querySelector('body');
 const observer = new MutationObserver(() => {
-  const baneer = document.querySelector('.evd-wrapper .block')
-  const desc = document.querySelector('.evd-wrapper > .evd-desc')
-  const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
-  desc.insertBefore(baneer, child)
+  if(document.querySelector('.evd-wrapper > .block') & document.querySelector('.evd-wrapper > .evd-desc') & document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')) {
+    const baneer = document.querySelector('.evd-wrapper > .block')
+    const desc = document.querySelector('.evd-wrapper > .evd-desc')
+    const child = document.querySelector('.evd-wrapper > .evd-desc > .evd-desc')
+    desc.insertBefore(baneer, child)
+  }
 });
 const config = { childList: true };
 observer.observe(target, config);
